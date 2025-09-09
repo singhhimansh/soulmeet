4eac88b335f0227744c63fb9b90d390eb6f7bd95
singhhimansh <him.signum802@gmail.com>
2025-09-09 14:37:18 +0530

    url changes

--- web/src/utils/constants.js
+++ web/src/utils/constants.js
@@ -1,5 +1,3 @@
-// export const BASE_URL =
-//   location.hostname === "localhost" ? "http://localhost:3000" : "/api";
-
-  export const BASE_URL =
-  location.hostname === "localhost" ? "http://localhost:3000" : "/api";
+export const BASE_URL = "https://soulmeet-server.vercel.app"
+  // export const BASE_URL =
+  // location.hostname === "localhost" ? "http://localhost:3000" : "/api";
