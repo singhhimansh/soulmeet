6095c21de3fc2fde10b28ffba6f8edcd0e2ac70e
singhhimansh <him.signum802@gmail.com>
2025-09-10 19:21:26 +0530

    fixes

--- web/src/utils/constants.js
+++ web/src/utils/constants.js
@@ -1,3 +1,3 @@
-export const BASE_URL = mport.meta.env.VITE_SERVER_URL;
+export const BASE_URL = import.meta.env.VITE_SERVER_URL;
   // export const BASE_URL =
   // location.hostname === "localhost" ? "http://localhost:3000" : "/api";
